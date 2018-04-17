f1d58c9dadf12ea08818965bb02a82db392ea11e
Lucas Porter-Bakker <lucas.porter.bakker@gmail.com>
2018-04-17 18:41:10 +0800

    seach feature working, where you can search by language and title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
+
   def home
+    @gifs = Gif.all
   end
+
+  def search
+    if params[:query].present?
+      sql_query = "title ILIKE :query OR language ILIKE :query"
+      @gifs = Gif.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @gifs = Gif.all
+    end
+    @query = params[:query]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
-  get 'gifs/index'
-
-  get 'gifs/show'
-
-  get 'gifs/new'
-
-  get 'gifs/create'
-
-  get 'gifs/edit'
-
-  get 'gifs/update'
-
-  get 'gifs/destroy'
 
   devise_for :users
   resources :gifs
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/search', to: 'pages#search', as: 'search'
+
 end
